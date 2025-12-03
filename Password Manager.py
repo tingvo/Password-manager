bcd17247a746b754f0bdb55482bb11f893eefa17
tingvo <tom.ingvorsen@gmail.com>
2025-12-03 13:26:08 +0000

    database added

--- Password Manager.py
+++ Password Manager.py
@@ -1,9 +1,19 @@
 import sqlite3 as sql
 from tkinter import *
+from tkinter import ttk
+
+conn = sql.connect('passwords.db')
+cursor = conn.cursor()
 
 root = Tk()
-e = Entry(root).pack()
+title = Label(root, text="Password Manager").pack()
 
-accounts = []
+cursor.execute("SELECT account FROM passwords")
+accounts = cursor.fetchall()
+
+acc_sel = ttk.Combobox(root, values = accounts)
+acc_sel.set("Select an account")
+acc_sel.pack()
+
 
 root.mainloop()
