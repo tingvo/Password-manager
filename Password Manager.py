16cfe4b7bb8c52e4514d481a82b6ca01633e3e5d
tingvo <tom.ingvorsen@gmail.com>
2025-12-03 21:07:03 +0000

    added more widgets

--- Password Manager.py
+++ Password Manager.py
@@ -1,12 +1,11 @@
 import sqlite3
 from tkinter import *
-from tkinter import ttk
 
 conn = sqlite3.connect('passwords.db')
 cursor = conn.cursor()
 
 root = Tk()
-root.geometry("210x110")
+root.geometry("400x210")
 root.title("Password Manager")
 
 def show():
@@ -14,7 +13,7 @@ def show():
     details = cursor.fetchall()
     for x in details:
         account = x[0]
-        if account == acc_sel.get():
+        if account == acc_sel.get(ACTIVE):
             username = x[1]
             password = x[2]
     userLabel.config(text="Username: " + username)
@@ -23,15 +22,33 @@ def show():
 cursor.execute("SELECT account FROM passwords")
 accounts = cursor.fetchall()
 
-acc_sel = ttk.Combobox(root, values = accounts)
-acc_sel.set("Select an account")
-acc_sel.grid(row=0, column=0)
+acc_sel = Listbox(root)
+for item in accounts:
+    acc_sel.insert(END, item)
+acc_sel.grid(row=0, column=1, rowspan=6)
 
-show_button = Button(root, padx=70, text="Enter", command=show).grid(row=1, column=0)
-userLabel = Label(root, text=" ")
-userLabel.grid(row=2, column=0)
-passLabel = Label(root, text=" ")
-passLabel.grid(row=3, column=0)
+acc_entry = Entry(root)
+acc_entry.grid(row=3, column=0)
+acc_entry.insert(0, "Add Website")
+
+user_entry = Entry(root)
+user_entry.grid(row=4, column=0)
+user_entry.insert(0, "Add Username")
+
+pass_entry = Entry(root)
+pass_entry.grid(row=5, column=0)
+pass_entry.insert(0, "Add Password")
+
+conf_entry = Entry(root, text="Confirm Password")
+conf_entry.grid(row=6, column=0)
+conf_entry.insert(0, "Confirm Password")
+
+show_button = Button(root, padx=37, text="Show Details", command=show)
+show_button.grid(row=6, column=1)
+userLabel = Label(root, text="Username: ", anchor="w")
+userLabel.grid(row=0, column=0)
+passLabel = Label(root, text="Password: ", anchor="w")
+passLabel.grid(row=1, column=0)
 
 root.mainloop()
 conn.close()
