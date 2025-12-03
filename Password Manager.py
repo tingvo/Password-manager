175a69b9147f2235c96fa09aad8c65e1de6b37b8
tingvo <tom.ingvorsen@gmail.com>
2025-12-03 15:17:45 +0000

    variable issues

--- Password Manager.py
+++ Password Manager.py
@@ -1,19 +1,36 @@
-import sqlite3 as sql
+import sqlite3
 from tkinter import *
 from tkinter import ttk
 
-conn = sql.connect('passwords.db')
+conn = sqlite3.connect('passwords.db')
 cursor = conn.cursor()
 
 root = Tk()
-title = Label(root, text="Password Manager").pack()
+root.geometry("300x200")
+root.title("Password Manager")
+
+def show():
+    cursor.execute("SELECT * FROM passwords")
+    details = cursor.fetchall()
+    for x in details:
+        account = x[0]
+        if account == acc_sel.get():
+            username = x[1]
+            password = x[2]
+    userLabel = Label(root, text="Username: " + username).pack()
+    passLabel = Label(root, text="Password: " + password).pack()
 
 cursor.execute("SELECT account FROM passwords")
 accounts = cursor.fetchall()
+
+clicked = StringVar()
 
 acc_sel = ttk.Combobox(root, values = accounts)
 acc_sel.set("Select an account")
 acc_sel.pack()
 
+show_button = Button(root, text="Enter", command=show()).pack()
 
+conn.commit()
+conn.close()
 root.mainloop()
